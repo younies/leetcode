0f98d65bbf75e5de2857c96088269425b2cfd338
Younies Mahmoud <younies.mahmoud@gmail.com>
2023-11-25 03:04:45 +0100

    solve two problems

---
+++ 2.add-two-numbers.rs
@@ -0,0 +1,110 @@
+/*
+ * @lc app=leetcode id=2 lang=rust
+ *
+ * [2] Add Two Numbers
+ *
+ * https://leetcode.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (41.59%)
+ * Likes:    29074
+ * Dislikes: 5643
+ * Total Accepted:    4.1M
+ * Total Submissions: 9.8M
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order, and each of their nodes
+ * contains a single digit. Add the two numbers and return the sum as a linked
+ * list.
+ * 
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in each linked list is in the range [1, 100].
+ * 0 <= Node.val <= 9
+ * It is guaranteed that the list represents a number that does not have
+ * leading zeros.
+ * 
+ * 
+ */
+
+// @lc code=start
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let (mut l1, mut l2) = (l1, l2);
+        let mut head = Some(Box::new(ListNode::new(0)));
+        let mut current = head.as_mut();
+        let mut overflow = 0;
+
+        while l1.is_some() || l2.is_some() {
+            let mut sum = overflow;
+            if let Some(node) = l1 {
+                sum += node.val;
+                l1 = node.next;
+            }
+
+            if let Some(node) = l2 {
+                sum += node.val;
+                l2 = node.next;
+            }
+
+            overflow = sum / 10;
+            if let Some(cur) = current {
+                cur.next = Some(Box::new(ListNode::new(sum % 10)));
+                current = cur.next.as_mut();
+            }
+        }
+
+        if overflow > 0 {
+            current.unwrap().next = Some(Box::new(ListNode::new(overflow)));
+        }
+
+        head.unwrap().next
+    }
+}
+// @lc code=end
+
